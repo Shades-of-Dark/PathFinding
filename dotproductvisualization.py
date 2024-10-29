69e06c6cc675d42cc090dfda6e907b61966946d4
Shades-of-Dark <raovedh@gmail.com>
2024-10-28 18:08:20 -0700

    cool beans

--- dotproductvisualization.py
+++ dotproductvisualization.py
@@ -1,9 +1,8 @@
 import sys
 
 import pygame
+from math import *
 
-from enemy import Enemy
-from player import Player
 
 sys.path.insert(0, '..')
 from pixelartfontsystem.pixelfont import *
@@ -14,6 +13,49 @@ screen = pygame.display.set_mode((500, 500), 0, 32)
 
 clock = pygame.time.Clock()
 font = Font("font.png")
+
+class Enemy():
+    def __init__(self):
+        self.rect = pygame.Rect(225, 225, 50, 50)
+        self.movement = pygame.Vector2(1, 0)
+
+
+    def update(self):
+        pass
+
+    def draw(self, surf):
+        pygame.draw.line(surf, (255, 0, 0), (250, 250), (450, 250), 4)
+
+
+class Player():
+    def __init__(self):
+        self.rect = pygame.Rect(50, 100, 50, 50)
+        self.movement = pygame.Vector2(1, 0)
+        self.speed = 5
+        self.angle = 0
+
+    def update(self, enemyvec):
+        mx, my = pygame.mouse.get_pos()
+        distY = my - 250
+        hypotenuselength = sqrt((mx - 250) ** 2 + distY ** 2)
+
+        self.angle = asin(distY/hypotenuselength)
+        if mx < 250:
+            self.angle = pi - self.angle
+
+        self.movement.x = cos(self.angle)
+        self.movement.y = sin(self.angle)
+
+        self.rect.center = (200 * self.movement.x + 250, 200 * self.movement.y + 250)
+        self.desirability = self.movement.dot(enemyvec)
+        self.movement.normalize()
+
+
+    def draw(self, surf):
+        pygame.draw.line(surf, (0, 255, 0), (250, 250), self.rect.center, 4)
+        pygame.draw.arc(surf, (255, 255, 255), pygame.Rect(225, 225, 50, 50), 0, -self.angle, 2)
+
+
 
 goodGuy = Player()
 badGuy = Enemy()
--- player.py
+++ player.py
@@ -1,31 +1,3 @@
 import pygame
 from math import *
 
-
-class Player():
-    def __init__(self):
-        self.rect = pygame.Rect(50, 100, 50, 50)
-        self.movement = pygame.Vector2(1, 0)
-        self.speed = 5
-        self.angle = 0
-
-    def update(self, enemyvec):
-        mx, my = pygame.mouse.get_pos()
-        distY = my - 250
-        hypotenuselength = sqrt((mx - 250) ** 2 + distY ** 2)
-
-        self.angle = asin(distY/hypotenuselength)
-        if mx < 250:
-            self.angle = pi - self.angle
-
-        self.movement.x = cos(self.angle)
-        self.movement.y = sin(self.angle)
-
-        self.rect.center = (200 * self.movement.x + 250, 200 * self.movement.y + 250)
-        self.desirability = self.movement.dot(enemyvec)
-        self.movement.normalize()
-
-
-    def draw(self, surf):
-        pygame.draw.line(surf, (0, 255, 0), (250, 250), self.rect.center, 2)
-        pygame.draw.arc(surf, (255, 255, 255), pygame.Rect(225, 225, 50, 50), 0, -self.angle, 2)
